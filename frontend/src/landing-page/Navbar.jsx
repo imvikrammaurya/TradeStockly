855efbdd5adf93aa643f517d9dfc10a0be986672
Vikram Maurya <imvikrammaurya@outlook.com>
2025-11-30 23:46:04 +0530

    Nav bar is created and designed exectly like the orginal

--- frontend/src/landing-page/Navbar.jsx
+++ frontend/src/landing-page/Navbar.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 export default function Navbar() {
+  const ROUTES = ["Signup", "About", "Products", "Pricing", "Support"];
+
   return (
-    <nav>
-      <a href="#">
-        <img
-          src="public\media\images\logo_svg.svg"
-          className="w-[10%]"
-          alt=""
-        />
-      </a>
+    <nav className="fixed top-0 left-0 w-full z-50 bg-white border-b border-gray-100">
+      <div className="my-2 container flex items-center justify-between mx-auto">
+        {/* Nav Logo */}
+        <div className="relative-section">
+          <a href="#">
+            <img
+              src="public\media\images\vikrammaurya_logo.png"
+              className="w-[30%] px-10 ml-40"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <ul className="flex flex-row gap-3 mr-50 text-gray-600">
+          {ROUTES.map((routes, i) => {
+            return (
+              <li className="m-4" key={routes}>
+                {routes}
+              </li>
+            );
+          })}
+          <li className="mt-4.5 ml-4">
+            <RxHamburgerMenu size={25} />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
