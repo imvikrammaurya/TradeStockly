ebcffacd3e62c737afa03454c662ab27c3a4303c
Vikram Maurya <imvikrammaurya@outlook.com>
2025-12-14 21:54:49 +0530

    Working on routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./landing-page/home/HomePage.jsx";
+import Signup from "./landing-page/signup/Signup.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/Signup" element={<Signup />}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
