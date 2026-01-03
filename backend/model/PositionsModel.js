4789a4ccd76ddc5a3bf5f0b0274040d1bd5f8dd2
Vikram Maurya <imvikrammaurya@outlook.com>
2026-01-03 22:48:20 +0530

    Model and Schema is done for orders and positions in database

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,7 @@
+const { model } = require("mongoose");
+
+const { PositionsSchema } = require("../schemas/PositionsSchema");
+
+const PositionsModel = new model("postion", PositionsSchema);
+
+module.exports = { PositionsModel };
