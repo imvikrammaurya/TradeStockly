f1b611192fd00a8d9bd60bc3f54e347c9a78f0cc
Vikram Maurya <imvikrammaurya@outlook.com>
2026-01-03 22:35:15 +0530

    Model and Schema is done for holdings setion in backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
 
 const app = express();
 
@@ -6,6 +12,10 @@ app.get("/", (req, res) => {
   res.send("Server is ready!");
 });
 
-app.listen(3002, () => {
+app.listen(PORT, () => {
   console.log("app is running at port 3002");
+  mongoose
+    .connect(uri)
+    .then(() => console.log("DB Connected!"))
+    .catch((err) => console.log(err));
 });
